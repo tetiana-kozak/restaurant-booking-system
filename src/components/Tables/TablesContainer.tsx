f3b2429feacbdd6d09c16f7bcefc5a6a540799dc
Tetiana <kozaktanya077@gmail.com>
2023-07-30 15:05:03 +0300

    feat: move tablesArray to staticData file

--- src/components/Tables/TablesContainer.tsx
+++ src/components/Tables/TablesContainer.tsx
@@ -1,26 +1,16 @@
+import { tablesArray } from 'utils/staticData'
 import './Tables.scss'
 
 type Props = {}
 
 const TablesContainer = (props: Props) => {
-  const tablesArray = [
-    { id: '1', positionX: '100px', positionY: '50px' },
-    { id: '2', positionX: '200px', positionY: '50px' },
-    { id: '3', positionX: '300px', positionY: '50px' },
-    { id: '4', positionX: '400px', positionY: '50px' },
-    { id: '5', positionX: '100px', positionY: '150px' },
-    { id: '6', positionX: '200px', positionY: '150px' },
-    { id: '7', positionX: '300px', positionY: '150px' },
-    { id: '8', positionX: '400px', positionY: '150px' },
-  ]
-
   return (
     <div className="tables-container">
       {tablesArray.map((table) => (
         <div
           className="table"
           key={table.id}
-          id={table.id}
+          id={table.id.toString()}
           style={{ left: table.positionX, top: table.positionY }}
         >
           <span className="table-id">{table.id}</span>
---
+++ src/utils/staticData.ts
@@ -0,0 +1,58 @@
+export const tablesArray = [
+  {
+    id: 1,
+    positionX: '100px',
+    positionY: '50px',
+    isBooked: false,
+    maxPeople: 2,
+  },
+  {
+    id: 2,
+    positionX: '200px',
+    positionY: '50px',
+    isBooked: false,
+    maxPeople: 4,
+  },
+  {
+    id: 3,
+    positionX: '300px',
+    positionY: '50px',
+    isBooked: false,
+    maxPeople: 4,
+  },
+  {
+    id: 4,
+    positionX: '400px',
+    positionY: '50px',
+    isBooked: false,
+    maxPeople: 2,
+  },
+  {
+    id: 5,
+    positionX: '100px',
+    positionY: '150px',
+    isBooked: false,
+    maxPeople: 6,
+  },
+  {
+    id: 6,
+    positionX: '200px',
+    positionY: '150px',
+    isBooked: false,
+    maxPeople: 6,
+  },
+  {
+    id: 7,
+    positionX: '300px',
+    positionY: '150px',
+    isBooked: false,
+    maxPeople: 10,
+  },
+  {
+    id: 8,
+    positionX: '400px',
+    positionY: '150px',
+    isBooked: false,
+    maxPeople: 10,
+  },
+]
