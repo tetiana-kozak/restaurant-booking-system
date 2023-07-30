2a98cefc4dc78f4a6659f4b92c6e024e4b11ec70
Tetiana <kozaktanya077@gmail.com>
2023-07-30 23:36:49 +0300

    add: booking form with formik

--- src/components/BookingForm/BookingForm.tsx
+++ src/components/BookingForm/BookingForm.tsx
@@ -1,5 +1,28 @@
+import { Formik, Form, Field } from 'formik'
+import './BookingForm.scss'
+
 type Props = {}
 const BookingForm = (props: Props) => {
-  return <div>BookingForm</div>
+  return (
+    <>
+      <div className="title">BookingForm</div>
+      <Formik
+        initialValues={{ name: '', phoneNumber: '' }}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions })
+        }}
+      >
+        <Form className="form-container">
+          <Field id="name" name="name" placeholder="Your Name" />
+          <Field
+            id="phoneNumber"
+            name="phoneNumber"
+            placeholder="Your Phone Number"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
+  )
 }
 export default BookingForm
