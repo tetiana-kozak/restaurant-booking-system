54526b1e3fc1d369e5e1ee8195eb06307e815034
Tetiana <kozaktanya077@gmail.com>
2023-07-30 18:31:17 +0300

    feat: change style of selected table

--- src/components/Tables/TablesContainer.tsx
+++ src/components/Tables/TablesContainer.tsx
@@ -1,17 +1,29 @@
 import { tablesArray } from 'utils/staticData'
 import './Tables.scss'
+import { useState } from 'react'
 
 type Props = {}
 
+type selectedTableType = {
+  id?: number
+}
+
 const TablesContainer = (props: Props) => {
+  const [selectedTable, setSelectedTable] = useState<selectedTableType>({})
+
   return (
     <div className="tables-container">
       {tablesArray.map((table) => (
         <div
-          className={'table ' + (table.isBooked ? 'booked' : 'free')}
+          className={
+            'table' +
+            (table.isBooked ? ' booked' : ' free') +
+            (table.id === selectedTable.id ? ' selected' : '')
+          }
           key={table.id}
           id={table.id.toString()}
           style={{ left: table.positionX, top: table.positionY }}
+          onClick={() => setSelectedTable({ id: table.id })}
         >
           <span className="table-id ">{table.id}</span>
         </div>
