c7e54e3d17c58679d05b81b9d85f274ea9af4e50
Tetiana <kozaktanya077@gmail.com>
2023-10-01 23:16:49 +0300

    feat: add theme colors in tilewind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    colors: {
+      primary: '#98CB6D',
+      secondary: '#D0E3C4',
+      tertiary: '#EFF4EA',
+      'button-primary': '#FFB859',
+      'button-secondary': '#503047',
+      error: '#CE2020',
+      'text-color': '#292929',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
