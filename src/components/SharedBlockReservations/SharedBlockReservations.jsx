8cf587b10bc4a07a2219e64a4430d2b328784694
AllaKaplia <kaplia.alla18@gmail.com>
2023-11-11 00:39:45 +0200

    add styles for SharedBlockReservations

--- src/components/SharedBlockReservations/SharedBlockReservations.jsx
+++ src/components/SharedBlockReservations/SharedBlockReservations.jsx
@@ -3,14 +3,25 @@ import { InfoIcon } from "icons/Info";
 
 const SharedBlockReservations = () => {
     return (
-        <div className="max-width: 1392px mx-auto py-12 px-6 bg-iframe_bg_color">
-            <h2>Забронюйте столик онлайн</h2>
-            <p>Забудьте про довгі черги та незручні дзвінки.</p>
-            <p>Наш онлайн-сервіс дозволить вам здійснити бронювання в будь-який час доби, навіть коли ресторан закритий.</p>
-            <p>Звісно, ви можете забронювати столик як гість, але щоб мати можливість відстежувати свої бронювання столиків та користуватися всіма перевагами нашого сервісу, будь ласка, увійдіть в свій особистий аккаунт або зареєструйтесь.</p>
-            <button type="button">Реєстрація</button>
-            <button type="button">Вхід</button>
-            <span> <InfoIcon /> Інформація для рестораторів</span>
+        <div className="w-1392 mx-auto pt-60 pb-70 px-50 bg-iframe_bg_color rounded-md">
+            <div className="w-552">
+                <h2 className="font-sans text-h2 text-text-color mb-50">Забронюйте столик онлайн</h2>
+                <p className="font-sans text-p mb-30">Забудьте про довгі черги та незручні дзвінки.</p>
+                <p className="font-sans text-p mb-30">
+                    Наш онлайн-сервіс дозволить вам здійснити бронювання в будь-який час доби, навіть коли ресторан закритий.
+                </p>
+                <p className="font-sans text-p mb-50">
+                    Звісно, ви можете забронювати столик як гість, але щоб мати можливість відстежувати свої бронювання столиків та користуватися всіма перевагами нашого сервісу, будь ласка, увійдіть в свій особистий аккаунт або зареєструйтесь.
+                </p>
+            </div>
+            <div className="w-552 flex items-center justify-center gap-40 mb-264">
+                <button className="py-10 px-41 font-sans text-p leading-8 text-[#FFB859] cursor-pointer" type="button">Реєстрація</button>
+                <button className="font-sans text-p leading-8  cursor-pointer bg-button-primary " type="button">Вхід</button>
+            </div>
+            <div className="w-552 flex items-center flex-start gap-20">
+                <InfoIcon />
+                <p className="font-sans text-p"> Інформація для рестораторів</p>
+            </div>
         </div>
     )
 };
