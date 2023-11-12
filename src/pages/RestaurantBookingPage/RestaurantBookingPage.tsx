7353b6c25b4e188cee5c2e5da080c27e4a8a54b5
AllaKaplia <kaplia.alla18@gmail.com>
2023-11-12 16:05:32 +0200

    change RestaurantBookingPage

--- src/pages/RestaurantBookingPage/RestaurantBookingPage.tsx
+++ src/pages/RestaurantBookingPage/RestaurantBookingPage.tsx
@@ -1,12 +1,5 @@
-import SharedBlockReservations from "components/SharedBlockReservations/SharedBlockReservations";
-
 type Props = {}
 const RestaurantBookingPage = (props: Props) => {
-  return (
-    <div className="max-width: 1440px mx-auto py-12 px-6">
-      <SharedBlockReservations />
-    </div>
-  )
-};
-
-export default RestaurantBookingPage;
+  return <div>RestaurantBookingPage</div>
+}
+export default RestaurantBookingPage
