a929f943570e5a4e996ef7a1bc7479ca1bc4477a
Tetiana <kozaktanya077@gmail.com>
2023-11-18 22:50:10 +0200

    add: useUnauthorizedRedirect add TODO

--- src/utils/axios/configureAxios.ts
+++ src/utils/axios/configureAxios.ts
@@ -1,8 +1,5 @@
-import axios, {
-  AxiosError,
-  AxiosResponse,
-  InternalAxiosRequestConfig,
-} from 'axios'
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const configureAxios = axios.create({
   baseURL: 'https://table-flow-fca566db5b85.herokuapp.com/api/v1',
@@ -30,20 +27,25 @@ const getErrorCode = (error: AxiosError, codeToCheck: number) => {
   // (error.response.data?.statusCode === codeToCheck)
 }
 
-const errorResponseInterceptor = (error: AxiosError) => {
-  const isUnauthorizedError = getErrorCode(error, 401)
-  if (isUnauthorizedError) {
-    // store.dispatch('auth/logout')
-    // router.replace({
-    //   path: '/signin',
-    //   query: { redirect: router.currentRoute.fullPath },
-    // })
+export const useUnauthorizedRedirect = () => {
+  const navigate = useNavigate()
+  const redirectToSignIn = () => {
+    navigate('/sign-in')
   }
-  return Promise.reject(error)
+  return { redirectToSignIn }
 }
 
 configureAxios.interceptors.request.use(authInterceptor)
 configureAxios.interceptors.response.use(
   (response) => response,
-  errorResponseInterceptor
+  (error: AxiosError) => {
+    const isUnauthorizedError = getErrorCode(error, 401)
+    if (isUnauthorizedError) {
+      localStorage.removeItem('token')
+      // TODO: Unauthorized Redirect to sign-in
+      // useUnauthorizedRedirect()
+    }
+
+    return Promise.reject(error)
+  }
 )
