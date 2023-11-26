0c542386223b9ed53674f32a7f75c36f33b3d5cd
AllaKaplia <kaplia.alla18@gmail.com>
2023-11-26 14:59:37 +0200

    Update SharedBlockReservations.jsx

--- src/components/SharedBlockReservations/SharedBlockReservations.jsx
+++ src/components/SharedBlockReservations/SharedBlockReservations.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import SharedBlockRegisteredUser from "components/SharedBlockRegisteredUser/SharedBlockRegisteredUser";
 import SharedBlockUnregisteredUser from "components/SharedBlockUnregisteredUser/SharedBlockUnregisteredUser";
 import SteppeReservation from "components/SteppeReservation/SteppeReservation";
 
 
 
-const SharedBlockReservations = ({ isUserLoggedIn }: { isUserLoggedIn: boolean }) => {
+const SharedBlockReservations = ({ isUserLoggedIn }) => {
     return (
         <div className="w-1392 mx-auto pt-60 pb-70 px-50 bg-iframe_bg_color rounded-md flex gap-52">
         {isUserLoggedIn ? (
