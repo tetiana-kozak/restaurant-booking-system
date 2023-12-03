6aa7f0d11e9dc0224efcab016564de60b901e942
AllaKaplia <kaplia.alla18@gmail.com>
2023-12-03 23:37:22 +0200

    Update ButtonTFMain.tsx

--- src/shared/buttons/ButtonTFMain/ButtonTFMain.tsx
+++ src/shared/buttons/ButtonTFMain/ButtonTFMain.tsx
@@ -1,7 +1,7 @@
-import { ButtonProps } from "types/buttonProps";
+import { buttonProps } from 'shared/types/buttonProps';
 import './ButtonTFMain.scss';
 
-const ButtonTFMain: React.FC<ButtonProps> = ({ onClick, label }) => {
+const ButtonTFMain: React.FC<buttonProps> = ({ onClick, label }) => {
     return (
         <button onClick={onClick} className='button_tf_main'>
             {label}
