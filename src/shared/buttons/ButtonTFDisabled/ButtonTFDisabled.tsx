2478060d8120fe16d5ccb5ec38cbdc28e2b3515b
AllaKaplia <kaplia.alla18@gmail.com>
2023-12-11 17:58:20 +0200

    Merge branch 'develop' into akaplia/feat/tf92/create_reusable_button

--- src/shared/buttons/ButtonTFDisabled/ButtonTFDisabled.tsx
+++ src/shared/buttons/ButtonTFDisabled/ButtonTFDisabled.tsx
@@ -1,12 +1,15 @@
-import { buttonProps } from 'shared/types/buttonProps';
-import './ButtonTFDisabled.scss';
 
-const ButtonTFDisabled: React.FC<buttonProps> = ({ onClick, label }) => {
+type Props = {
+    onClick: () => void
+    label: string
+}
+  
+const ButtonTFDisabled = ({ onClick, label }: Props) => {
     return (
-        <button onClick={onClick} className='button_tf_disabled'>
-            {label}
-        </button>
+      <button onClick={onClick} className="font-sans text-p leading-8 cursor-pointer bg-button-disabled py-10 px-41 rounded-100">
+        {label}
+      </button>
     )
-};
-
+}
+  
 export default ButtonTFDisabled;
