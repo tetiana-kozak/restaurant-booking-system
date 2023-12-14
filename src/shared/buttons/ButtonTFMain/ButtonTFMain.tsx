79d6f084e41b4d03c613813a0fd24f906afd7e0d
AllaKaplia <kaplia.alla18@gmail.com>
2023-12-14 09:25:49 +0200

    Update ButtonTFMain.tsx

--- src/shared/buttons/ButtonTFMain/ButtonTFMain.tsx
+++ src/shared/buttons/ButtonTFMain/ButtonTFMain.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ButtonTFMain = ({ onClick, label }: Props) => {
     return (
-        <button onClick={onClick} className='font-sans text-p text-text-color leading-8 cursor-pointer bg-button-primary py-13 px-30 rounded-100 hover:shadow-hover-btn focus:shadow-hover-btn active:shadow-hover-btn transition-btn-transition'>
+        <button onClick={onClick} className="font-sans text-p text-text-color leading-8 cursor-pointer bg-button-primary py-13 px-30 rounded-100 hover:shadow-hover-btn focus:shadow-hover-btn active:shadow-hover-btn transition-btn-transition">
             {label}
         </button>
     )
