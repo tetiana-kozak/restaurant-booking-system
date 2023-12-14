1a03dcf0e180d342a440f34165ec9dba470110bc
AllaKaplia <kaplia.alla18@gmail.com>
2023-12-14 09:19:54 +0200

    update all

--- src/shared/buttons/ButtonTFDisabled/ButtonTFDisabled.tsx
+++ src/shared/buttons/ButtonTFDisabled/ButtonTFDisabled.tsx
@@ -6,7 +6,7 @@ type Props = {
   
 const ButtonTFDisabled = ({ onClick, label }: Props) => {
     return (
-      <button onClick={onClick} className="font-sans text-p text-text-color leading-8 cursor-pointer bg-button-disabled py-13 px-30 rounded-100 hover:shadow-hover-btn focus:shadow-hover-btn active:shadow-hover-btn transition-btn-transition">
+      <button onClick={onClick} className="font-sans text-p text-text-color leading-8 cursor-pointer bg-button-disabled py-13 px-30 rounded-100 hover:shadow-hover-btn active:shadow-hover-btn focus:shadow-hover-btn transition-btn-transition">
         {label}
       </button>
     )
