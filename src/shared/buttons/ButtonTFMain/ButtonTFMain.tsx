be974896aacb7b909fa995d7027e6f7eb8fa83e3
Tetiana <kozaktanya077@gmail.com>
2023-12-17 20:54:20 +0200

    fix: button style

--- src/shared/buttons/ButtonTFMain/ButtonTFMain.tsx
+++ src/shared/buttons/ButtonTFMain/ButtonTFMain.tsx
@@ -1,15 +1,17 @@
-
 type Props = {
-    onClick: () => void
-    label: string
-  }
+  onClick?: () => void
+  label: string
+}
 
 const ButtonTFMain = ({ onClick, label }: Props) => {
-    return (
-        <button onClick={onClick} className="font-sans text-p text-text-color leading-8 cursor-pointer bg-button-primary py-13 px-30 rounded-100 hover:shadow-hover-btn focus:shadow-hover-btn active:shadow-hover-btn transition-btn-transition">
-            {label}
-        </button>
-    )
-};
+  return (
+    <button
+      onClick={onClick}
+      className="text-p text-text-color leading-8 cursor-pointer bg-button-primary py-13 px-30 rounded-100 hover:shadow-hover-btn hover:bg-hover-btn-primary transition-all"
+    >
+      {label}
+    </button>
+  )
+}
 
-export default ButtonTFMain;
+export default ButtonTFMain
