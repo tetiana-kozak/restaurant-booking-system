45436aae92b4051cac0c056b82a1eb332f3dd231
AllaKaplia <kaplia.alla18@gmail.com>
2024-01-01 02:12:52 +0200

    create StepsTabs

--- src/shared/stepsTabs/StepsTabs/StepsTabs.tsx
+++ src/shared/stepsTabs/StepsTabs/StepsTabs.tsx
@@ -1,7 +1,36 @@
-import Box from '@mui/material/Box'
 
-const StepsTabs = () => {
-  return <Box sx={{ width: '100%' }}>stepper</Box>
+type Props = {
+  steps: string[];
+  activeStep: number;
 }
 
-export default StepsTabs
+const StepTabs = ({ steps, activeStep }:Props) => {
+
+  return (
+    <div className="w-665">
+      <div>
+        {steps.map((step, index) => {
+          let buttonClass = 'stepper';
+          if (index < activeStep) {
+            buttonClass = 'stepper';
+          } else if (index === activeStep) {
+            buttonClass = 'button-secondary';
+          } else if (index > activeStep) {
+            buttonClass = 'secondary';
+          }
+          return (
+            <button
+              type="button"
+              className={`py-10 px-20 w-164 font-sans text-p text-text-color bg-${buttonClass}`}
+              disabled={index > activeStep}
+            >
+              {step}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default StepTabs;
