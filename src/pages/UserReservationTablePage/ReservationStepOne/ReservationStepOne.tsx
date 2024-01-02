5631eb6f09e327a9bb4d4740f46f18141c444d7a
AllaKaplia <kaplia.alla18@gmail.com>
2024-01-02 23:28:23 +0200

    update ReservationStepOne

--- src/pages/UserReservationTablePage/ReservationStepOne/ReservationStepOne.tsx
+++ src/pages/UserReservationTablePage/ReservationStepOne/ReservationStepOne.tsx
@@ -2,24 +2,27 @@ import { useState } from "react";
 import ButtonTFDisabled from "shared/buttons/ButtonTFDisabled/ButtonTFDisabled";
 import ButtonTFMain from "shared/buttons/ButtonTFMain/ButtonTFMain";
 import StepsTabs from 'shared/stepsTabs/StepsTabs/StepsTabs';
+import BasicDateCalendar from "../BasicDateCalendar/BasicDateCalendar";
 
 const ReservationStepOne = () => {
     const [currentStep, setCurrentStep] = useState<number>(0);
     const steps = ['Крок 1', 'Крок 2', 'Крок 3', 'Крок 4'];
     const activeStep = 0;
+
     const clickNext = () => {
-  
+
     };
   
     const clickTurnBack = () => {
-  
+      
     }
 
     return(
-        <div>
+      <div>
         <StepsTabs steps={steps} activeStep={activeStep}/>
+        <BasicDateCalendar />
         {currentStep > 0 ? (
-          <ButtonTFDisabled onClick={clickTurnBack} label="Повернутись" />
+          <ButtonTFDisabled onClick={clickTurnBack} label="Повернутись"/>
         ) : null}
         <ButtonTFMain onClick={clickNext} label="Підтвердити"/>
       </div>
