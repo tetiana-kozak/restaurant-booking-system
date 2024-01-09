4c35e675fe3793fc7ec78f188b252c2b65ec37a8
AllaKaplia <kaplia.alla18@gmail.com>
2024-01-09 14:18:57 +0200

    add logic for steps ReservationStepOne

--- src/pages/UserReservationTablePage/ReservationStepOne/ReservationStepOne.tsx
+++ src/pages/UserReservationTablePage/ReservationStepOne/ReservationStepOne.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import ButtonTFDisabled from "shared/buttons/ButtonTFDisabled/ButtonTFDisabled";
 import ButtonTFMain from "shared/buttons/ButtonTFMain/ButtonTFMain";
 import StepsTabs from 'shared/stepsTabs/StepsTabs/StepsTabs';
@@ -7,14 +8,25 @@ import BasicDateCalendar from "../BasicDateCalendar/BasicDateCalendar";
 const ReservationStepOne = () => {
     const [currentStep, setCurrentStep] = useState<number>(0);
     const steps = ['Крок 1', 'Крок 2', 'Крок 3', 'Крок 4'];
-    const activeStep = 0;
+    const [activeStep, setActiveStep] = useState<number>(0);
 
     const clickNext = () => {
+      const isDateSelected = true; //Якщо обрана дата і час на календарі - ще треба змінити!
+
+      if(isDateSelected){
+        setCurrentStep(currentStep + 1);
+        setActiveStep(activeStep + 1);
+      } else if(activeStep === 3){
+
+      } else {
+        toast.info('Оберіть зручну дату в календарі!');
+      }
 
     };
   
     const clickTurnBack = () => {
-      
+      setCurrentStep(currentStep - 1);
+      setActiveStep(activeStep - 1);
     }
 
     return(
